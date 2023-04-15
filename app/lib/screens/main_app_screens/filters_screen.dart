60e8a573730072fb0c2e07a9c99635dc1d90e556
Jose Evans <jpjoao24@hotmail.com>
2023-04-15 18:19:03 +0100

    developing search filters

--- app/lib/screens/main_app_screens/filters_screen.dart
+++ app/lib/screens/main_app_screens/filters_screen.dart
@@ -1,7 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:itflowapp/constants/constants.dart';
 
-class FilterScreen extends StatelessWidget {
+class FilterScreen extends StatefulWidget {
+  @override
+  _FilterScreenState createState() => _FilterScreenState();
+}
+
+class _FilterScreenState extends State<FilterScreen> {
+  bool isPartTimeSelected = false;
+  bool isFullTimeSelected = false;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -9,9 +17,88 @@ class FilterScreen extends StatelessWidget {
         title: Text(appName),
         centerTitle: true,
       ),
-      body: Center(
-        child: Text('Apply filters here'),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 32, horizontal: 16),
+            child: Text(
+              'Filters',
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(bottom: 8),
+                  child: Text(
+                    'Company name',
+                    style: TextStyle(fontSize: 16),
+                  ),
+                ),
+                TextField(
+                  decoration: InputDecoration(
+                    hintText: 'Search company',
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    contentPadding: const EdgeInsets.symmetric(
+                        vertical: 12, horizontal: 16),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 16, bottom: 8),
+                  child: Text(
+                    'Type of Job',
+                    style: TextStyle(fontSize: 16),
+                  ),
+                ),
+                Row(
+                  children: [
+                    Expanded(
+                      child: ElevatedButton(
+                        onPressed: () {
+                          setState(() {
+                            isPartTimeSelected = true;
+                            isFullTimeSelected = false;
+                          });
+                        },
+                        child: Text('Part time'),
+                        style: ElevatedButton.styleFrom(
+                          primary: isPartTimeSelected
+                              ? Colors.green
+                              : Colors.grey[300],
+                        ),
+                      ),
+                    ),
+                    SizedBox(width: 16),
+                    Expanded(
+                      child: ElevatedButton(
+                        onPressed: () {
+                          setState(() {
+                            isPartTimeSelected = false;
+                            isFullTimeSelected = true;
+                          });
+                        },
+                        child: Text('Full time'),
+                        style: ElevatedButton.styleFrom(
+                          primary: isFullTimeSelected
+                              ? Colors.green
+                              : Colors.grey[300],
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
 }
+
